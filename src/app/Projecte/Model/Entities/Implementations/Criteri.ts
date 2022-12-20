598f7a9b10269df2c1f4c9960ab4c1b92cbdff37
janEstrada24 <jestrada48@boscdelacoma.cat>
2022-12-20 19:40:36 +0100

    Constructors creats

--- src/app/Projecte/Model/Entities/Implementations/Criteri.ts
+++ src/app/Projecte/Model/Entities/Implementations/Criteri.ts
@@ -4,4 +4,8 @@ import { IValoracio } from "../Interfaces/IValoracio";
 export class Criteri implements ICriteri<IValoracio> {
     nom!: string;
     valoracions: Array<IValoracio> = new Array<IValoracio>();
+    constructor(nom:string, valoracions: Array<IValoracio>) {
+        this.nom = nom;
+        this.valoracions = valoracions;
+    }
 }
--- src/app/Projecte/Model/Entities/Implementations/Valoracio.ts
+++ src/app/Projecte/Model/Entities/Implementations/Valoracio.ts
@@ -3,4 +3,10 @@ import { IValoracio } from "../Interfaces/IValoracio";
 export class Valoracio implements IValoracio {
     nom!: string;
     num!: number;
+
+    constructor(nom:string, num:number) {
+        this.nom = nom;
+        this.num = num;
+    }
+
 }
